16f468fd0ec0f0cb3807fbe40b62eeb9f1e4cea7
Shailendra_iOSDev <shailendra4077@gmail.com>
2022-01-09 23:39:43 +0530

    Add files via upload

---
+++ Card_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_AnimationApp.swift
+//  Card Animation
+//
+//  Created by shailendra singh on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Card_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
